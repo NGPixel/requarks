015615007752489f3ba024285ec0bbbc0b0e76b5
NGPixel <github@ngpixel.com>
2016-04-02 11:52:28 -0400

    Added base setup for tests

---
+++ tests/setup-db.js
@@ -0,0 +1,14 @@
+var  chai = require('chai'),
+	  expect = chai.expect;
+
+describe('setup-db', function () {
+
+	describe('mssql', function () {
+
+		it('must connect', function() {
+			expect(true).to.be.true;
+		});
+
+	});
+
+});
