97641faaa8450ddf6c36522bbac7b13c10f06c3f
NGPixel <github@ngpixel.com>
2016-06-24 18:56:21 -0400

    Fixed travis env variables + config load error

--- tests/index.js
+++ tests/index.js
@@ -9,45 +9,52 @@ let path = require('path'),
 
 global._ = require('lodash');
 global.winston = require('winston');
-global.appconfig = require('../config.json', _.noop, (err) => {
 
-	// Use default test values
+// ========================================
+// Load configuration values
+// ========================================
 
-	global.appconfig = {
-	   "db": {
-	      "connstr": "mongodb://localhost/requarks"
-	   },
-	   "storage": {
-	      "local": {
-	         "path": path.join(process.env.HOME, './requarksdata/')
-	      },
-	      "provider": "local"
-	   },
-	   "redis": {
-	      "config": "noauth",
-	      "host": "localhost",
-	      "port": "6379"
-	   },
-	   "auth0": {},
-	   "title": "Requarks",
-	   "host": "http://localhost",
-	   "setup": true,
-	   "testenv": true,
-	   "sessionSecret": "1234567890abcdefghijklmnopqrstuvxyz"
+fs.access('../config.json', fs.R_OK, (err) => {
+	if(err) {
+
+		// Use default test values
+
+		global.appconfig = {
+		   "db": {
+		      "connstr": "mongodb://localhost/requarks"
+		   },
+		   "storage": {
+		      "local": {
+		         "path": path.join(process.env.HOME, './requarksdata/')
+		      },
+		      "provider": "local"
+		   },
+		   "redis": {
+		      "config": "noauth",
+		      "host": "localhost",
+		      "port": "6379"
+		   },
+		   "auth0": {},
+		   "title": "Requarks",
+		   "host": "http://localhost",
+		   "setup": true,
+		   "sessionSecret": "1234567890abcdefghijklmnopqrstuvxyz"
+		}
+		
+		fs.mkdirSync(appconfig.storage.local.path);
+
+	} else {
+
+		// Use current values
+
+		global.appconfig = require('../config.json');
+
 	}
 
+	// ========================================
+	// Run Tests
+	// ========================================
+
+	require('./setup-db.js');
+
 });
-
-// ========================================
-// Test CI specifics
-// ========================================
-
-if(appconfig.testenv) {
-	fs.mkdirSync(appconfig.storage.local.path);
-}
-
-// ========================================
-// Run Tests
-// ========================================
-
-require('./setup-db.js');
