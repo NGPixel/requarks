d7a25d09f726a56d20b1dcbb64db597310a094f2
NGPixel <github@ngpixel.com>
2016-06-24 21:15:10 -0400

    Fix test config import issues

--- tests/index.js
+++ tests/index.js
@@ -14,47 +14,49 @@ global.winston = require('winston');
 // Load configuration values
 // ========================================
 
-fs.access('../config.json', fs.R_OK, (err) => {
-	if(err) {
+try {
 
-		// Use default test values
+	// can't use async here, doing it the ugly way...
 
-		global.appconfig = {
-		   "db": {
-		      "connstr": "mongodb://localhost/requarks"
-		   },
-		   "storage": {
-		      "local": {
-		         "path": path.join(process.env.HOME, './requarksdata/')
-		      },
-		      "provider": "local"
-		   },
-		   "redis": {
-		      "config": "noauth",
-		      "host": "localhost",
-		      "port": "6379"
-		   },
-		   "auth0": {},
-		   "title": "Requarks",
-		   "host": "http://localhost",
-		   "setup": true,
-		   "sessionSecret": "1234567890abcdefghijklmnopqrstuvxyz"
-		}
-		
-		fs.mkdirSync(appconfig.storage.local.path);
+	let configPath = path.join(__dirname, '../config.json');
 
-	} else {
+	fs.accessSync(configPath, fs.R_OK);
+	global.appconfig = require(configPath);
 
-		// Use current values
+} catch(err) {
 
-		global.appconfig = require('../config.json');
+	// Use default test values
 
+	let dataPath = (process.env.HOME) ? path.join(process.env.HOME, './requarksdata/') : path.join(__dirname, './tempdata/');
+
+	global.appconfig = {
+	   "db": {
+	      "connstr": "mongodb://localhost/requarks"
+	   },
+	   "storage": {
+	      "local": {
+	         "path": dataPath
+	      },
+	      "provider": "local"
+	   },
+	   "redis": {
+	      "config": "noauth",
+	      "host": "localhost",
+	      "port": "6379"
+	   },
+	   "auth0": {},
+	   "title": "Requarks",
+	   "host": "http://localhost",
+	   "setup": true,
+	   "sessionSecret": "1234567890abcdefghijklmnopqrstuvxyz"
 	}
+	
+	fs.mkdirSync(dataPath);
 
-	// ========================================
-	// Run Tests
-	// ========================================
+}
 
-	require('./setup-db.js');
+// ========================================
+// Run Tests
+// ========================================
 
-});
+require('./setup-db.js');
